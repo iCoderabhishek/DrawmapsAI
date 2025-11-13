3d5f2f92a114ee9aff468ed468c3bbe1e98268a7
Abhishek Jha <iamabhishek1310@gmail.com>
2025-11-13 21:33:54 +0530

    integrated auth to http backend

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,9 +1,11 @@
 import express from "express"
 import cors from "cors"
 import  UserRoute  from "./routes/user.routes.js"
-import SignUpRoute from "./routes/auth.routes.js"
+import AuthRoute from "./routes/auth.routes.js"
 import RoomRoute from "./routes/room.routes.js"
 import AIRoute from "./routes/ai.routes.js"
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "@repo/auth/auth";
 const app = express()
 
 app.use(express.json())
@@ -13,9 +15,10 @@ const router = express.Router()
 const HTTP_PORT = process.env.HTTP_PORT || 3001
 
 app.use("/api/v1/user", UserRoute)
-app.use("/api/v1/auth", SignUpRoute)
+app.use("/api/v1/auth", AuthRoute)
 app.use("/api/v1/rooms", RoomRoute)
 app.use("/api/v1/ai", AIRoute)
+app.all("/api/auth/*", toNodeHandler(auth));
 
 
 app.listen(HTTP_PORT, () => console.log("server is up @", HTTP_PORT))
